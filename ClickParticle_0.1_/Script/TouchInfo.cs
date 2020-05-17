ba6073fd829bfb716b3137791d7d951354fb3567
KENEW <32855863+KENEW@users.noreply.github.com>
2020-05-18 03:46:03 +0900

    Add files via upload

---
+++ ClickParticle_0.1_/Script/TouchInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInfo : MonoBehaviour
+{
+    private ResourcesLoad resLoad;
+    public SpriteRenderer renderSpr;
+
+    public void InitGame(GameObject gm)
+    {
+        renderSpr = gm.GetComponent<SpriteRenderer>();
+    }
+}
