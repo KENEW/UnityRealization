828abf4d6e63dfd4c1e9461a918cc9bc48de04a7
KENEW <32855863+KENEW@users.noreply.github.com>
2020-05-18 03:44:36 +0900

    Add files via upload

---
+++ ClickParticle_0.1_/ResourcesLoad.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourcesLoad : MonoBehaviour
+{
+    public static ResourcesLoad InstanceRes = null;
+
+    public Sprite[] MainSprite;
+    public Material[] MainMat;
+    public GameObject MainObj;
+    public GameObject MainSus;
+
+    private void Awake()
+    {
+        if (InstanceRes == null)
+        {
+            InstanceRes = this;
+
+        }
+        else if (InstanceRes != null)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        MainObj = Resources.Load("Objects/MainObject") as GameObject;
+        MainSus = Resources.Load<GameObject>("Objects/SusPart");
+
+        MainSprite = Resources.LoadAll<Sprite>("Sprites");
+        MainMat = Resources.LoadAll<Material>("Material");
+    }
+}
